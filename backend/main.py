55ad45e1161fff4e3b40d171c063db28630e7777
Srihas Inaganti <srihasinaganti@gmail.com>
2025-12-02 23:52:32 -0500

    added/reworked skeleton code of backend

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from mongo import db
+
+app = FastAPI()
+
+# ---- Group Endpoints ----
+
+
+
+# ---- Habit Endpoints ----
+
+
+
+# ---- Stock Endpoints ----
+
+
+
+# ---- User Endpoints ----
+
+
